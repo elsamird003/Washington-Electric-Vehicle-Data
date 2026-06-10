97577811bc89b3bebab4d02bfff24ec4e70ac56b
Samuel Hernandez <71301115+elsamird003@users.noreply.github.com>
2026-06-10 14:38:42 -0400

    was more descriptive on notes.mb

--- index.py
+++ index.py
@@ -5,14 +5,20 @@ import seaborn as sns
 data = pd.read_csv("asset/Data.csv")
 
 top_n = 30
-top_models = data["Make"].value_counts().nlargest(15)
-# subset = data[data["Make"].isin(top_models)]
+top_models = data["Model"].value_counts().nlargest(top_n).index
+subset = data[data["Model"].isin(top_models)]
 
-plt.figure(figsize=(10, 6))
-sns.barplot(x=top_models.values, y=top_models.index, palette="viridis")
-plt.title("Top 15 EV Manufacturers in Washington")
-plt.xlabel("Total Vehicles Registered")
-plt.ylabel("Manufacturer (Make)")
+plt.figure(figsize=(10, 8)) ## 10 witdth an 8 inches height
+plt.countplot(
+    y="Model",
+    data=subset,
+    order=top_models[::-1],  # highest count at top
+    color="red",
+)
+plt.
+plt.title(f"Top {top_n} EV models (Washington registrations)")
+plt.xlabel("Count")
+plt.ylabel("Model")
 plt.tight_layout()
 plt.show()
 
